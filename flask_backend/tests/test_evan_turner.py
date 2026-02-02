a2488cb61535a27da1851f316bf7a136c08c7aac
Evan <eboyct@gmail.com>
2026-02-02 12:09:42 -0800

    Create test_evan_turner.py
    Test doesn't pass yet

---
+++ flask_backend/tests/test_evan_turner.py
@@ -0,0 +1,24 @@
+import os, json
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+def test_evan_turner(test_client):
+    """
+    GIVEN GET /evan
+    WHEN the endpoint is accessed
+    THEN it should return a 200 status code and the expected information
+    """
+
+    # Make a GET request to the /practice/test endpoint
+    response = test_client.get("/practice/test")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    # Check that data is not None
+    assert data is not None
+
+    # Check for expected keys and values in the response
+    assert "course" in data
+    assert data["course"] == "cosc 224"
